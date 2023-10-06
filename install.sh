44fa46cd3df44be1584251bb3367ed9d127a57bc
Gamunu Balagalla <gamunu@fastcode.io>
2023-10-07 02:31:07 +0530

    feat: update install script

--- install.sh
+++ install.sh
@@ -1,67 +1,116 @@
 #!/bin/bash
 
-sudo pacman -Syu
-mkdir /tmp/yay && cd /tmp/yay
-curl -OJ 'https://aur.archlinux.org/cgit/aur.git/plain/PKGBUILD?h=yay'
-makepkg -si
-cd
-rm -rf /tmp/yay
+create_admin_user() {
+	# define user
+	local user="$1"
 
-yay --version
+	# check if the user already exists
+	if id "$user" >/dev/null 2>&1; then
+		echo "User $user already exists"
+		return
+	else
+		echo "Creating user $user"
+		# create the user
+		useradd -m -G wheel -s /bin/bash "$user" && passwd -d "$user"
+		# allow wheel group to run sudo without password
+		echo "$user ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers.d/"$user"
+	fi
+}
 
-git clone --depth=1 https://github.com/gamunu/dotfiles
+ipacman() {
+	pacman -S --needed --noconfirm $@
+}
 
-# install pacman packages
-yay -S --needed zsh neovim i3-wm feh i3lock rofi dunst ttf-font-awesome alacritty \
-# picom disabled for aur fork
-# install xserver
-xorg-server xorg-apps xorg-xinit xdotool \
-# simple greeter for i3-wm
-ly xorg-xauth libxcb \
-# XDG autostart
-# https://wiki.archlinux.org/title/XDG_Autostart
-dex \
-# Session lock
-# https://wiki.archlinux.org/title/Session_lock
-xss-lock \
-# Install i2c tools for backlight configuration
-ddcutil i2c-tools \
-# install applications
-spectacle dolphin nm-connection-editor blueman pavucontrol blueman-manager \
-okular perl-file-mimeinfo \
-# theme and icons and install lxappearance for gtk theme
-qt5ct breeze breeze-gtk lxappearance \
-# install starship prompt and tools
-starship exa bat \
-# documenations
-tldr \
-# install polybar
-polybar-scripts-git polybar-git ttf-segoe-ui-variable ttf-nerd-fonts-symbols-mono \
-# install greenclip for clipboard management
-rofi-greenclip picom-ftlabs-git
+iyay() {
+	yay -S --needed --noconfirm $@
+}
 
-# enable services
-sudo systemctl enable ly greenclip
+install_yay(){
+	ipacman git base-devel
+	# create builder user
+	useradd -m -G wheel builder && passwd -d builder
+	# allow wheel group to run sudo without password
+	mkdir -p /tmp/yay-build && chown -R builder:builder /tmp/yay-build
+	su - builder -c "git clone --depth 1 https://aur.archlinux.org/yay-bin.git /tmp/yay-build/yay"
+	su - builder -c "cd /tmp/yay-build/yay && makepkg -si --noconfirm"
+	rm -rf /tmp/yay-build
+}
 
-# copy home directory files
-mkdir -p ~/.config ~/.wallpapers ~/.local/share/fonts
-cp -r dotfiles/config ~/.config
-cp -r dotfiles/wallpapers ~/.wallpapers
-cp -r dotfiles/home/.* ~/
-cp -r dotfiles/fonts/* ~/.local/share/fonts
+install_i3() {
+	# install pacman packages
+	ipacman zsh neovim i3-wm feh i3lock rofi dunst ttf-font-awesome alacritty
+	# picom disabled for aur fork
+	# install xserver
+	ipacman xorg-server xorg-apps xorg-xinit xdotool
+	# simple greeter for i3-wm
+	ipacman ly xorg-xauth libxcb
+	# XDG autostart
+	# https://wiki.archlinux.org/title/XDG_Autostart
+	ipacman dex
+	# Session lock
+	# https://wiki.archlinux.org/title/Session_lock
+	ipacman xss-lock
+	# Install i2c tools for backlight configuration
+	ipacman ddcutil i2c-tools
+	# install applications
+	ipacman spectacle dolphin nm-connection-editor blueman pavucontrol blueman-manager \
+	okular perl-file-mimeinfo
+	# theme and icons and install lxappearance for gtk theme
+	ipacman qt5ct breeze breeze-gtk lxappearance
+	# install starship prompt and tools
+	ipacman starship exa bat
+	# documenations
+	ipacman tldr
+	# install polybar
+	iyay polybar-scripts-git polybar-git ttf-segoe-ui-variable ttf-nerd-fonts-symbols-mono
+	# install greenclip for clipboard management
+	iyay rofi-greenclip picom-ftlabs-git
+}
 
-# reload font cache
-fc-cache -f -v
+enable_services() {
+	# enable services
+	systemctl enable ly greenclip
+}
 
-# configure xdg settings
-xdg-mime default google-chrome-stable.desktop x-scheme-handler/https x-scheme-handler/http
-xdg-mime default org.kde.dolphin.desktop inode/directory
+cp_cfg() {
+	git clone --depth=1 https://github.com/gamunu/dotfiles /tmp/dotfiles
+	cd /tmp
+	# copy home directory files
+	mkdir -p ~/.config ~/.wallpapers ~/.local/share/fonts
+	cp -r dotfiles/config ~/.config
+	cp -r dotfiles/wallpapers ~/.wallpapers
+	cp -r dotfiles/home/.* ~/
+	cp -r dotfiles/fonts/* ~/.local/share/fonts
+	# reload font cache
+	fc-cache -f
 
-# configure fontconfig
-mkdir -p $HOME/.config/fontconfig/conf.d
+	rm -rf /tmp/dotfiles
+}
 
-# Enable font slight hinting
-ln -sf /usr/share/fontconfig/conf.avail/10-hinting-slight.conf $HOME/.config/fontconfig/conf.d
+cfg_xdg() {
+	# configure xdg settings
+	xdg-mime default google-chrome-stable.desktop x-scheme-handler/https x-scheme-handler/http
+	xdg-mime default org.kde.dolphin.desktop inode/directory
+}
 
-# enable dark theme for qt5
-cp /usr/share/color-schemes/BreezeDark.colors ~/.config/kdeglobals
+cfg_fonts() {
+	# configure fontconfig
+	mkdir -p $HOME/.config/fontconfig/conf.d
+	# Enable font slight hinting
+	ln -sf /usr/share/fontconfig/conf.avail/10-hinting-slight.conf $HOME/.config/fontconfig/conf.d
+}
+
+cfg_themes() {
+	# configure themes
+	# enable dark theme for qt5
+	cp /usr/share/color-schemes/BreezeDark.colors ~/.config/kdeglobals
+}
+
+create_admin_user "builder"
+install_yay
+install_i3
+enable_services
+cfg_xdg
+cfg_fonts
+cfg_themes
+cp_cfg
